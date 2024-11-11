807632959894f6035e3ac6f41d52faf2f3b30c7e
Bohdan Haidashchuk <firewhole97@gmail.com>
2024-11-11 19:41:20 +0200

    changes to database and tests

--- modules/common/database.py
+++ modules/common/database.py
@@ -3,7 +3,7 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.connection = sqlite3.connect(r'C://Users//haida//Desktop//local_repo//bohdan_qa_auto23' + r'become_qa_auto.db')
+        self.connection = sqlite3.connect(r'C://Users//haida//Desktop//local_repo//bohdan_qa_auto23//' + r'become_qa_auto.db')
         self.cursor = self.connection.cursor()
 
     def test_connection(self):
@@ -11,3 +11,42 @@ class Database:
         self.cursor.execute(sqlite_select_query)
         record = self.cursor.fetchall()
         print(f"Connection successfull. Data version is : {record}")
+
+    def get_all_users(self):
+        query = "SELECT name, address, city FROM customers"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+
+    def send_pachkage(self, name):
+        query = f"SELECT address, city, postalCode, country FROM customers WHERE name='{name}'"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
+
+    def update_sugar_info(self, product_id, qnt):
+        query = f"UPDATE products SET quantity = {qnt} where id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def select_sugar_product(self, product_id):
+        query = f"SELECT quantity FROM products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        record =self.cursor.fetchall()
+        return record
+    
+    def insert_info(self, product_id, name, description, quantity):
+        query = f"INSERT OR REPLACE INTO products(id,name,description,quantity) VALUES({product_id},'{name}','{description}',{quantity})"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def delete_product(self, product_id):
+        query = f"DELETE from products WHERE id = {product_id}"
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def get_info(self):
+        query = f"SELECT orders.id, customers.name, products.name, products.description, orders.order_date FROM orders JOIN customers ON orders.customer_id = customers.id JOIN products ON orders.product_id = products.id"
+        self.cursor.execute(query)
+        record = self.cursor.fetchall()
+        return record
--- tests/database/test_database.py
+++ tests/database/test_database.py
@@ -6,3 +6,50 @@ from modules.common.database import Database
 def test_database_connection():
     db = Database()
     db.test_connection()
+
+
+@pytest.mark.database
+def test_check_all_users():
+    db = Database()
+    users = db.get_all_users()
+    print(users)
+
+@pytest.mark.database
+def test_send_package():
+    db = Database()
+    user = db.send_pachkage('Sergii')
+
+    assert user [0][0] == 'Maydan Nezalezhnosti 1'
+    assert user [0][1] == 'Kyiv'
+    assert user [0][2] == '3127'
+    assert user [0][3] == 'Ukraine'
+
+@pytest.mark.database
+def test_product_update():
+    db = Database()
+    db.update_sugar_info(1, 25)
+    product_qnt = db.select_sugar_product(1)
+
+    assert product_qnt[0][0] == 25
+@pytest.mark.database
+def test_product_insert():
+    db=Database()
+    db.insert_info(4,'biscuit','sweet',34)
+    product_qnt = db.select_sugar_product(4)
+
+    assert product_qnt [0][0] == 34
+
+@pytest.mark.database
+def test_product_delete():
+    db=Database()
+    db.insert_info(99,'test','data',999)
+    db.delete_product(99)
+    qnt = db.select_sugar_product(99)
+
+    assert len(qnt) == 0
+@pytest.mark.database
+def test_check_join_info():
+    db=Database()
+    orders = db.get_info()
+    print("Orders", orders)
+    assert len(orders) == 1
